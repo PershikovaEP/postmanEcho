b50f3cc845b6e8ab13decc4dfd808406d5b365fd
PershikovaEP <zem524@mail.ru>
2022-08-06 01:45:41 +0300

    update test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -23,7 +23,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("some data"))
         ;
     }
 
